25760214da3c532129176923594f6b95c23b146e
TreeCoding <30642264+Bilibotter@users.noreply.github.com>
2018-10-05 19:23:25 +0800

    增加在有偏置情况下求解，运行后可得到效果图

--- PLA感知机.py
+++ PLA感知机.py
@@ -1,36 +1,123 @@
+import random
 import numpy as np
 from matplotlib import pyplot as plt
 
+grad = None
+b = None
+
 
 class PLA():
-    def dataSet(self, num=200, *args):
-        if not args:
-            b = np.random.normal(10, 10, 1)[0]
-            wegihts = np.random.normal(1, 1, 3)
-        else:
-            *wegihts, b = args
-        self.f_weights = wegihts
-        self.f_b = b
-        DS = []
-        dim = len(wegihts)
-        for i in range(num):
-            point = np.random.normal(b+5, 2, dim).tolist()
-            # 加入点的正负标签
-            point.append(np.sign(np.dot(point, wegihts)+b))
-            DS.append(point)
-        return DS
+    def fit(self, arr):
+        """
+        :param arr:
+        :return: 若返回True则在阈值内可收敛，False则不能
+        """
+        sign = lambda x: 1 if x > 0 else -1
+        err = []
+        n, m = arr.shape
+        self.n = n
+        self.m = m
+        m -= 1
+        constant = 0
+        threshold = 1000
+        weights = [1 for i in range(m)]
+        threshold = 1000
+        while threshold > 0:
+            for row in arr:
+                h = np.dot(weights, row[:m])
+                if sign(h+constant) != sign(row[-1]):
+                    threshold -= 1
+                    p = random.uniform(0, 1)
+                    weights = weights + p * row[-1] * row[:m]
+                    constant = -1 * (h * p + constant * (1 - p))
+                    err.append(row)
+            if not err:
+                self.weights = weights
+                self.constant = constant
+                return True
+            while err:
+                row = err.pop(0)
+                h = np.dot(weights, row[:m])
+                if sign(h+constant) != sign(row[-1]):
+                    threshold -= 1
+                    p = random.uniform(0, 1)
+                    weights = weights + p * row[-1] * row[:m]
+                    constant = -1 * (h + constant) / 2
+                    err.append(row)
+                    if threshold < 0:
+                        self.weights = weights
+                        self.constant = constant
+                        return False
+
+        return True
+
+    def predict(self, arr):
+        sign = lambda x: 1 if x > 0 else -1
+        m = self.m - 1
+        return np.array([sign(np.dot(self.weights, row[:m])+self.constant) for row in arr])
+
+    def check(self, arr, weights, constant):
+        sign = lambda x: 1 if x > 0 else -1
+        m = self.m - 1
+        return np.array([sign(np.dot(weights, row[:m]) + constant) for row in arr])
 
 
-p = PLA()
-DS = p.dataSet()
-# point = []*4 为浅拷贝
-points = [[] for i in range(4)]
-for point in DS:
-    span = 0 if point[-1] > 0 else 2
-    points[span].append(point[0])
-    points[span+1].append(point[1])
+# 随意选择，参数是为了图好看
+def dataSet(w_1, w_2, turns=200):
+    global b
+    global grad
+    constant = 20
+    b = constant
+    print('Fact:', w_1, w_2, constant)
+    r = lambda: random.choice((1, -1))*np.random.normal(constant+8, 3, 1)[0]
+    w_x = lambda x_1, x_2: w_1*x_1+w_2*x_2
+    grad = (w_1, w_2)
+    constant *= -1
+    for i in range(turns):
+        x = r()
+        y = r()
+        f = w_x(x, y)
+        if f > constant:
+            yield [x, y, 1]
+        elif f != constant:
+            yield [x, y, -1]
 
-plt.scatter(points[0], points[1], c='b')
-plt.scatter(points[-2], points[-1], c='r', marker='^')
-plt.plot((-p.f_b/p.f_weights[0], 0), (0, -p.f_b/p.f_weights[-1]))
+
+R = lambda: random.randint(-10, 10)
+w_1, w_2, w_3 = R(), R(), R()
+DS = np.array([vec for vec in dataSet(w_1, w_2)])
+vm = PLA()
+print('Separable?', vm.fit(DS))
+vm.predict(DS)
+p_preds = vm.predict(DS)
+weights = vm.weights
+constant = vm.constant
+print('Hypothesis:', weights, constant)
+positive = [[], []]
+negative = [[], []]
+errs = 0
+for row, pred in zip(DS, p_preds):
+    if row[-1] != pred:
+        print(weights*row[:vm.m-1]+constant, pred, row)
+        errs += 1
+    if pred == 1:
+        positive[0].append(row[0])
+        positive[-1].append(row[1])
+    else:
+        negative[0].append(row[0])
+        negative[-1].append(row[1])
+print(errs)
+print('{:.2%}'.format(errs/200))
+y_2_x = lambda gradint, x, c: -(gradint[0]/gradint[-1])*x-c/gradint[-1]
+f_line = [[], []]
+h_line = [[], []]
+for i in (-22, 22):
+    f_line[0].append(i)
+    f_line[-1].append(y_2_x(grad, i, b))
+    h_line[0].append(i)
+    h_line[-1].append(y_2_x(weights, i, constant))
+plt.scatter(positive[0], positive[-1], c='b')
+plt.scatter(negative[0], negative[-1], c='r', marker='^')
+plt.plot(f_line[0], f_line[-1], c='b')
+plt.plot(h_line[0], h_line[-1], c='r')
 plt.show()
